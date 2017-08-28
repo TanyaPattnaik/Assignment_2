af1e43d5c0ce5ad2c41fd2aaff5c2c42d6b842ed
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-08-28 17:20:26 +0530

    Create Lab2_question9.cpp
    C++ program to calculate area of an equilateral triangle

---
+++ Lab2_question9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+double s,ar;
+cout << "Enter side of equilateral triangle\n";
+cin >> s;
+ar=(1.732/4.0)*s*s;
+cout << "Area of the traingle is "<<ar <<"\n";
+return 0;
+}
