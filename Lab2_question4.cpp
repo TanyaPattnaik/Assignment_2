881e1d91bcc7f1fe9b82fa2b405a07cd53f76f16
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-08-28 16:55:11 +0530

    Update Lab2_question4.cpp

--- Lab2_question4.cpp
+++ Lab2_question4.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 int main()
 {
-int d,d2,y,w;
+int d,r,y,w;
 cout << "Enter number of days\n";
 cin >> d;
-d2=d;
 y=d/365;
-d=d-(y*365);
-w=d/7;
-d=d-(w*7);
-cout << d2 <<" days is "<< y <<" year(s) "<< w <<" week(s) and "<< d <<" day(s).\n";
+r=d%365;
+w=r/7;
+r=r%7;
+cout << d <<" days is "<< y <<" year(s) "<< w <<" week(s) and "<< r <<" day(s).\n";
 return 0;
 }
