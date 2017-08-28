e19bb701f28bf745e40bd74b8a3c6c35abdec57d
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-08-28 16:44:09 +0530

    Create Lab2_question5.cpp
    C++ program to find power of any number x ^ y

---
+++ Lab2_question5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+int x,y,z;
+cout<< "Enter base and exponent\n";
+cin >> x >> y;
+z=pow(x,y);
+cout<< "Required number=" <<z<<"\n";
+return 0;
+}
