7c379489ddc14158d80f197b5a1c884901e4dfba
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-08-28 16:48:31 +0530

    Create Lab2_question6.cpp
    C++ program to enter any number and calculate its square root

---
+++ Lab2_question6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+double x,y;
+cout<< "Enter number\n";
+cin >> x;
+y=sqrt(x);
+cout<< "Required square root=" <<y<<"\n";
+return 0;
+}
