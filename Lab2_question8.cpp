99766d9a8fa3324f2f3e84ea1d717fd642924341
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-08-28 17:16:46 +0530

    Create Lab2_question8.cpp
    C++ program to enter base and height of a triangle and find its area

---
+++ Lab2_question8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+double h,b,ar;
+cout << "Enter height and base of triangle\n";
+cin >> h>> b;
+ar=0.5*b*h;
+cout << "Area of the traingle is "<<ar <<"\n";
+return 0;
+}
